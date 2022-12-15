af3ae55f93f94f9e2953894f6c22c7415d8f3cac
Agnes Septilia <90135656+agnes-septilia@users.noreply.github.com>
2022-12-15 17:23:41 +0700

    Update final file

--- mongodb/mongodb_etl_zips.py
+++ mongodb/mongodb_etl_zips.py
@@ -5,25 +5,30 @@ from sqlalchemy import create_engine
 
 
 ### connect to MongoDB and take necessary collections
-client = MongoClient("mongodb+srv://mongo_agnes_user:dev@cluster0.54gwtdp.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("mongodb://mongo_agnes_user:dev@ac-ccoug6t-shard-00-00.54gwtdp.mongodb.net:27017,ac-ccoug6t-shard-00-01.54gwtdp.mongodb.net:27017,ac-ccoug6t-shard-00-02.54gwtdp.mongodb.net:27017/?ssl=true&replicaSet=atlas-bzzp8q-shard-0&authSource=admin&retryWrites=true&w=majority")
 sample_training = client["sample_training"]
 zips_coll = sample_training["zips"]
 
 
+### For collection zips, take all columns and flatten the loc field to become latitude and longitude 
 
-### TASK 1: for collection zips, take all columns and flatten the loc field to become latitude and longitude 
+### DATA PROCESSING
 
+# create empty dataframe to capture the result of data transformation
 sample_training_zips = pd.DataFrame()
 
-for field in zips_coll.find():
+
+for field in zips_coll.find().limit():
     zips_column = []
     zips_value = []
     
     for key, value in field.items():
+        # convert ObjectID  as id
         if key == '_id':
             zips_column.append("id")
             zips_value.append(str(value))
         
+        # flatten loc array to latitude and longitude
         elif key == "loc":
             for k, v in value.items():
                 if k == "y":
@@ -32,20 +37,27 @@ for field in zips_coll.find():
                 else:
                     zips_column.append("longitude")
                     zips_value.append(v)
+        
+        # take other fields as it is
         else:
             zips_column.append(key)
             zips_value.append(value)
     
+    # convert dictionary as dataframe row
     df_value = pd.DataFrame([zips_value], columns = zips_column)
+
+    # append row to main dataframe
     sample_training_zips = pd.concat([sample_training_zips,df_value])
 
 # print(sample_training_zips.head())
 
 
 
+### UPLOAD DATA TO POSTGRES
+
 # connection to postgres
 url = 'postgresql://postgres:1234@localhost:5432/postgres'
 engine = create_engine(url)
 
 # export data to postgres
-sample_training_zips.to_sql('sample_training_zips', index=False, con=engine, schema='final_project', if_exists='replace')
+sample_training_zips.to_sql('sample_training_zips_test', index=False, con=engine, schema='final_project', if_exists='replace')
