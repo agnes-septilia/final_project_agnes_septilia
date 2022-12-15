3ebccfbc6c19906548b4738c8302069e4674fc0d
Agnes Septilia <90135656+agnes-septilia@users.noreply.github.com>
2022-12-15 18:53:03 +0700

    Update correct final file

--- airflow/dags/dag_facts_table_monthly.py
+++ airflow/dags/dag_facts_table_monthly.py
@@ -1,25 +1,18 @@
 # import libraries
 from datetime import datetime
 from datetime import timedelta
-from airflow import DAG
+from airflow import DAG, macros
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.postgres_operator import PostgresOperator
 from airflow.operators.python import PythonOperator
 from airflow.sensors.external_task import ExternalTaskSensor
 from airflow.models import DagRun
-from airflow.utils.task_group import TaskGroup
 from airflow.utils.dates import days_ago 
 
 
 # function to get parent-dag most recent execution result
-def most_recent_mongodb(dt):
-    dag_runs = DagRun.find(dag_id="etl_mongodb")
-    dag_runs.sort(key=lambda x: x.execution_date, reverse=True)
-    if dag_runs:
-        return dag_runs[0].execution_date
-
-def most_recent_spark(dt):
-    dag_runs = DagRun.find(dag_id="etl_spark")
+def most_recent_dims(dt):
+    dag_runs = DagRun.find(dag_id="dim_tables")
     dag_runs.sort(key=lambda x: x.execution_date, reverse=True)
     if dag_runs:
         return dag_runs[0].execution_date
@@ -30,10 +23,11 @@ default_args = {
 	'owner': 'Admin'
 }
 
+
 with DAG(
-    "dim_tables",
+    "fact_monthly",
     start_date = days_ago(1),
-    schedule_interval = None,
+    schedule_interval = '0 17 L * *',   # set to match timezone utc +7 00:00, set to every end-of-month
     default_args = default_args
 ) as dag:
 
@@ -41,57 +35,23 @@ with DAG(
     job_start = DummyOperator(
         task_id = "job_start"
         )
-        
+    
+    # Create dependencies from dag dim_tables
+    dim_tables = ExternalTaskSensor(
+        task_id = 'dim_tables_task',
+        external_dag_id = 'dim_tables',
+        external_task_id = 'job_finish',
+        execution_date_fn = most_recent_dims,
+        allowed_states = ['success']
+        )
 
-    # Create dependencies from other dags
-    with TaskGroup("other_dag_dependencies") as other_dag_dependencies:
-        # get dag etl_mongodb result that had succeed earlier
-        mongodb_task = ExternalTaskSensor(
-            task_id = 'mongodb_task',
-            external_dag_id = 'etl_mongodb',
-            external_task_id = 'job_finish',
-            execution_date_fn = most_recent_mongodb,
-            allowed_states = ['success']
-            )
-
-        # get dag etl_spark result that had succeed earlier
-        spark_task = ExternalTaskSensor(
-            task_id = 'spark_task',
-            external_dag_id = 'etl_spark',
-            external_task_id = 'job_finish',
-            execution_date_fn = most_recent_spark,
-            allowed_states = ['success']
-            )
-
-
-    # Run dim_country sql script
-    create_dim_country = PostgresOperator(
-        task_id = "create_dim_country",
+    # Run fact table daily_avg_currency sql script
+    create_fact_monthly_avg_currency = PostgresOperator(
+        task_id = 'create_fact_monthly_avg_currency',
         postgres_conn_id = "airflow_final_project",
-        sql = open("/home/agnes/Documents/digital_skola/Project/final_project/dimension_table/dim_country.sql").read()
+        sql = open("/home/agnes/Documents/digital_skola/Project/final_project/fact_table/fact_monthly_avg_currency.sql").read()
     )
-
-    # Run dim_state sql script
-    create_dim_state = PostgresOperator(
-        task_id = "create_dim_state",
-        postgres_conn_id = "airflow_final_project",
-        sql = open("/home/agnes/Documents/digital_skola/Project/final_project/dimension_table/dim_state.sql").read()
-    )
-
-    # Run dim_city sql script
-    create_dim_city = PostgresOperator(
-        task_id = "create_dim_city",
-        postgres_conn_id = "airflow_final_project",
-        sql = open("/home/agnes/Documents/digital_skola/Project/final_project/dimension_table/dim_city.sql").read()
-    )
-
-    # Run dim_currency sql script
-    create_dim_currency = PostgresOperator(
-        task_id = "create_dim_currency",
-        postgres_conn_id = "airflow_final_project",
-        sql = open("/home/agnes/Documents/digital_skola/Project/final_project/dimension_table/dim_currency.sql").read()
-    ) 
- 
+  
     # Finish job
     job_finish = DummyOperator(
         task_id = "job_finish"
@@ -101,10 +61,7 @@ with DAG(
     # Orchestration
     (
         job_start
-        >> other_dag_dependencies
-        >> create_dim_country
-        >> create_dim_state
-        >> create_dim_city
-        >> create_dim_currency
+        >> dim_tables
+        >> create_fact_monthly_avg_currency
         >> job_finish
     )
