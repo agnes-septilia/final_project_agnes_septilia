a3d4272db89b1c4a7b944aabde81f8f9184d1676
Agnes Septilia <90135656+agnes-septilia@users.noreply.github.com>
2022-12-15 17:51:24 +0700

    Update final file

--- fact_table/fact_monthly_avg_currency.sql
+++ fact_table/fact_monthly_avg_currency.sql
@@ -1,14 +1,13 @@
 -- Create fact monthly average currency
-drop table if exists final_project.fact_monthly_avg_currency; 
 
-create table final_project.fact_monthly_avg_currency (
+create table if not exists final_project.fact_monthly_avg_currency (
 	currency_id varchar,
 	currency_name varchar,
 	end_of_month date,
 	avg_rate float
 );
 
-insert into final_project.fact_daily_avg_currency (
+insert into final_project.fact_monthly_avg_currency (
 	currency_id,
 	currency_name,
 	end_of_month,
@@ -18,11 +17,11 @@ insert into final_project.fact_daily_avg_currency (
 select 
 	currency_id,
 	currency_name,
-	date_trunc('month', "timestamp") + interval '1 month' - interval '1 day' as end_of_month,
+	date_trunc('month', to_timestamp("timestamp",'YYYY-MM-DD HH24:MI:SS')) + interval '1 month' - interval '1 day' as end_of_month,
 	avg(rate) as avg_rate
 from final_project.topic_currency
-where date_trunc('month', "timestamp") = date_trunc('month', {{ macros.ds }}::date) - interval '1 month'
-group by currency_id , currency_name , date_trunc('month', "timestamp")
+where date_trunc('month', to_timestamp("timestamp",'YYYY-MM-DD HH24:MI:SS')) = date_trunc('month', '{{ execution_date }}'::date - interval '1 day')
+group by currency_id , currency_name , date_trunc('month', to_timestamp("timestamp",'YYYY-MM-DD HH24:MI:SS'))
 order by currency_id
 )
 ;
